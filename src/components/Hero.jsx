d12bc8713c7c913bcf5655a8c8d9e568fca291aa
Your_Name <musharafgujjar7711@gmail.com>
2024-10-30 08:37:02 +0500

    done change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -59,16 +59,16 @@ function Hero() {
                             Start Project
                         </button>
                     </div>
-                    <div className='flex gap-8 mt-5'>
-                        <div className='flex flex-col items-center'>
+                    <div className='flex gap-8 mt-5 justify-center'>
+                        <div className='flex flex-col md:items-center gap-2'>
                             <h1 className='font-bold text-xl md:text-5xl'>2+</h1>
                             <p className='text-xs font-bold text-gray-500'>Years Experience</p>
                         </div>
-                        <div className='flex flex-col items-center'>
+                        <div className='flex flex-col md:items-center gap-2'>
                             <h1 className='font-bold text-xl md:text-5xl'>{project}k</h1>
                             <p className='text-xs font-bold text-gray-500'>Projects Success</p>
                         </div>
-                        <div ref={ref} className='flex flex-col items-center'>
+                        <div ref={ref} className='flex flex-col md:items-center gap-2'>
                             <h1 className='font-bold text-xl md:text-5xl'>{count}%</h1>
                             <p className='text-xs font-bold text-gray-500'>Satisfied Ratio</p>
                         </div>
@@ -80,19 +80,19 @@ function Hero() {
                     <img
                         src={myProfile}
                         alt="Portfolio showcase"
-                        className="max-w-xs md:max-w-md rounded-xl transform scale-125 opacity-80 hover:opacity-100 transition duration-300"
+                        className="max-w-xs md:max-w-md ml-9 md:ml-0  rounded-xl transform scale-125 opacity-80 hover:opacity-100 transition duration-300"
                     />
 
 
                    
-                        <div className='bird absolute top-20 md:top-44 sm:top-40 right-72 sm:right-28 md:right-12 space-y-14'>
+                        <div className='bird absolute top-20 md:top-44 sm:top-40 right-52 sm:right-28 md:right-12 space-y-14'>
                             <div className='relative bg-white rounded-full text-xs md:text-lg font-medium py-2 px-5'>
                                 Illustration
                                 <PiTwitterLogoDuotone className='absolute text-2xl md:text-5xl text-teal-50 bg-teal-400 py-2 px-2 rounded-full -top-4 -left-3 md:-top-7' />
                             </div>
                         </div>
 
-                        <div className='bird2 absolute top-36 md:top-60 mt-12 right-72 sm:right-32 sm:top-60 md:right-0 space-y-14'>
+                        <div className='bird2 absolute top-36 md:top-60 mt-12 right-60 sm:right-32 sm:top-60 md:right-0 space-y-14'>
                             <div className='relative bg-white rounded-full text-xs md:text-lg font-medium py-2 px-5'>
                                 Web Developer
                                 <PiTwitterLogoDuotone className='absolute text-2xl md:text-5xl text-teal-50 bg-teal-400 py-2 px-2 rounded-full -top-4 -left-3 md:-top-7' />
